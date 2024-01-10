a3dc2b05eb8b4838b9204c9683db0d57c7c500f4
ellenisheleme <92553479+ellenisheleme@users.noreply.github.com>
2024-01-10 01:57:08 +0000

    Implements isPasswordComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -10,4 +10,32 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public static Boolean isEven(int n){
+            return n % 2 ==0;
+    }
+
+    public static boolean verifyPassword(String password){
+
+        return password.length()>= 6 && password.matches(".*\\d.*") 
+               && password.matches(".*[a-z]*.")
+               && password.matches(".*[A-Z]*.");
+        // if(password.length() < 6){
+        //     return false;
+        // }
+        // boolean hasUpperCase = false;
+        // boolean hasLowerCase = false;
+        // boolean hasDigit = false;
+
+        // for(int i = 0; i< password.length() || !hasUpperCase && !hasLowerCase && !hasDigit; i++){
+        //     char current = password.charAt(i);
+        //     if(Character.isDigit(current)){
+        //         hasDigit = true;
+        //     }else if (Character.isUpperCase(current)){
+        //             hasUpperCase = true;
+        //     }else if(Character.isLowerCase(current)){
+        //         hasLowerCase = true;
+        //     }
+        // }
+        // return hasDigit && hasLowerCase && hasUpperCase;
+    }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,7 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +18,23 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldCheckEvenNumber(){
+        assertEquals(false, App.isEven(5));
+    }
+
+    @Test 
+    public void shouldVerifyPassword(){
+        assertEquals(true, App.verifyPassword("1PaleWis"));
+        assertEquals(true, App.verifyPassword("5POSETDet"));
+        assertEquals(true, App.verifyPassword("P3TidET"));
+        assertEquals(true, App.verifyPassword("elenSeem4"));
+        assertEquals(true, App.verifyPassword("525T87s0"));
+        assertEquals(true, App.verifyPassword("5POSETDeT"));
+        
+
+
     }
 }
